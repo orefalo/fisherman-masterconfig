59125b0c7894bb6f10baec59d6db6a827d5a352f
Olivier Refalo <orefalo@yahoo.com>
2018-06-24 09:07:40 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# masterconfig
+
+[![Build Status][travis-badge]][travis-link]
+[![Slack Room][slack-badge]][slack-link]
+
+This fish extension brings balance to the force.
+It installs all my other fish dependencies, sets my key aliases and settings
+
+One command to run, to set it all
+
+## Install
+
+With [fisherman]
+
+```
+fisher orefalo/masterconfig
+```
+
+## Usage
+
+```fish
+masterconfig
+```
+
+[travis-link]: https://travis-ci.org/orefalo/masterconfig
+[travis-badge]: https://img.shields.io/travis/orefalo/masterconfig.svg
+[slack-link]: https://fisherman-wharf.herokuapp.com
+[slack-badge]: https://fisherman-wharf.herokuapp.com/badge.svg
+[fisherman]: https://github.com/fisherman/fisherman
---
+++ conf.d/masterconfig.fish
@@ -0,0 +1,46 @@
+if status --is-interactive
+
+    # not used anymore 'fisher grc' takes care of it
+    #set grc_wrap_commands "cat cvs df diff dig gcc g++ ifconfig make mount mtr netstat ping ps tail traceroute wdiff"
+
+    # colors
+    set -x LSCOLORS 'GxfxhxhxcxegedaCaCgxgx'
+    set -x CLICOLOR 1
+    set -x MAVEN_COLOR true
+    set -x LESS "--RAW-CONTROL-CHARS"
+    set -x GREP_OPTIONS '--color=auto'
+    set -x GREP_COLOR '1;33'
+
+    # my key aliases
+    alias j=z
+    alias ll="ls -lh"
+    alias ls="ls -color"
+    alias dps="grc docker ps"
+    alias dpi="grc docker images"
+    alias dc=docker-compose
+    alias pgstart="brew services start postgresql"
+    alias pgstop="brew services stop postgresql"
+    alias pgtop=pg_top
+
+    # fixes my typos
+    alias gti=git
+    alias sl=ls
+
+    # specific osx aliases
+    if test (uname) = "Darwin"
+        set -x EDITOR "subl -w"
+        alias top=htop
+        alias preview="open -a '$PREVIEW'"
+        alias mou="open -a MacDown"
+        alias jd="open -a JD-GUI"
+    end
+
+    # Setup the subl alias to SublimeText
+    if not test -L /usr/local/bin/subl
+           and test -f "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"
+       rm -f /usr/local/bin/subl
+       ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
+    end
+
+end
+
