044d8b7a0287eb71e0734a731a8c6e84b5351bbf
Olivier Refalo <orefalo@yahoo.com>
2019-11-05 12:32:22 +0400

    ch

--- conf.d/masterconfig.fish
+++ conf.d/masterconfig.fish
@@ -162,17 +162,16 @@ if status --is-interactive
         brew cask install kubernetic
         brew cask install aerial
         brew cask install iina
-        brew cask install qlcolorcode # Preview source code files with syntax highlighting
-        brew cask install qlstephen # Preview plain text files without a file extension. Example: README, CHANGELOG, etc.
-        brew cask install qlmarkdown # Preview Markdown files
-        brew cask install quicklook-json # Preview JSON files
-        brew cask install qlprettypatch # Preview .patch files
-        brew cask install quicklook-csv # Preview CSV files
-        brew cask install qlimagesize # Display image size and resolution
-        brew cask install webpquicklook # Preview WebP images
+        # broken with catalina
+        # brew cask install qlcolorcode # Preview source code files with syntax highlighting
+        # brew cask install qlstephen # Preview plain text files without a file extension. Example: README, CHANGELOG, etc.
+        # brew cask install qlmarkdown # Preview Markdown files
+        # brew cask install quicklook-json # Preview JSON files
+        # brew cask install qlprettypatch # Preview .patch files
+        # brew cask install quicklook-csv # Preview CSV files
+        # brew cask install qlimagesize # Display image size and resolution
+        # brew cask install webpquicklook # Preview WebP images
         brew cask install suspicious-package # Preview the contents of a standard Apple installer package
-
-
 
         #if test ! -x (which fisher)
             # install fisher if not installed
