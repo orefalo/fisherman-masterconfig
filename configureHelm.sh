7155f51d9c61092d9fc508bec58adfd0feb22c44
olivier <orefalo@gmail.com>
2022-01-08 20:36:01 +0400

    fx

--- configureHelm.sh
+++ configureHelm.sh
@@ -5,7 +5,6 @@ if test ! -x (which brew)
     /bin/bash -c "(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 end
 
-
 helm plugin install https://github.com/futuresimple/helm-secrets
 helm plugin install https://github.com/databus23/helm-diff --version master 
 # like npm-check but for charts
@@ -13,4 +12,3 @@ helm plugin install https://github.com/fabmation-gmbh/helm-whatup
 
 # terraform prettyfier/colorizer
 go get -u github.com/dmlittle/scenery
-
--- install_npm_packages.sh
+++ install_npm_packages.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env fish
 # source it from fish
 
+npm i -g yarn
+
 for i in (cat npm-packages.txt)
 
 	if test (string sub -l 1 "$i") = "#"
 		echo Skipping $i
 	else if test "(string length $i)" != "0"
 		echo $i
-		npm install -g (string trim $i)
+		yarn global add (string trim $i)
 	end
 
 end
-
-
