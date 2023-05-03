c25b2c99e9c4e88f91a8e5ad17bebfb7fc7a2d90
Olivier Refalo <orefalo@snapcore.com>
2023-05-03 15:02:28 +0400

    brew

--- configureHelm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-helm plugin install https://github.com/futuresimple/helm-secrets
-helm plugin install https://github.com/databus23/helm-diff --version master 
-# like npm-check but for charts
-helm plugin install https://github.com/fabmation-gmbh/helm-whatup
-
-# terraform prettyfier/colorizer
-go get -u github.com/dmlittle/scenery
