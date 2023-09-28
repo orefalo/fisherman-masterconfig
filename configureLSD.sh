08d71e0fd87e448add128af9a838f6f96c4ccefe
olivier <orefalo@gmail.com>
2023-09-28 15:57:05 +0400

    fix lsd

--- configureLSD.sh
+++ configureLSD.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-mkdir -p ~/.config/lsd/
-cp configs/lsd/*.yaml ~/.config/lsd/
+mkdir -p ~/.config/lsd/themes
+cp configs/lsd/config.yaml ~/.config/lsd/
+cp configs/lsd/dracula.yaml ~/.config/lsd/themes/
